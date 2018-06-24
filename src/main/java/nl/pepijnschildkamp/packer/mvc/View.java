a25f4bdda6c0f0d52a53c77c2fc0846563758331
Pepijn Schildkamp <pepijnschildkamp@pepijns-mbp.home>
2018-06-24 15:19:39 +0200

    Better layout

--- src/main/java/nl/pepijnschildkamp/packer/mvc/View.java
+++ src/main/java/nl/pepijnschildkamp/packer/mvc/View.java
@@ -3,10 +3,8 @@ package nl.pepijnschildkamp.packer.mvc;
 import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
-
-import static java.awt.Component.LEFT_ALIGNMENT;
-import static javax.swing.BoxLayout.PAGE_AXIS;
 
 public class View {
 
@@ -15,126 +13,137 @@ public class View {
     private JComboBox timeOutComboBox;
     private JButton addRowButton;
     private JButton removeRowButton;
-    private JButton packButton;
-    private JTable boxesTable;
+    private JButton packButton = new JButton("temp");
+    private JTable boxesTable = new JTable(); //temp
 
     public View() {
-        frame = new JFrame("Mainfreight - Packing optimalisator");
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        frame = createFrame();
+        Container container = frame.getContentPane();
+
+        JPanel panel = new JPanel(new GridLayout(4, 1, 0, 10));
+
+        panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        /* Add header */
+        panel.add(getHeaderPanel());
+
+        /* add container selection at left column */
+        panel.add(getPackerPanel());
+
+        /* right column */
+        panel.add(getBoxesPanel());
+
+        panel.add(getOptionsPanel());
+        container.add(panel);
+    }
+
+    private JFrame createFrame() {
+        JFrame frame = new JFrame("Mainfreight - Packing optimalisator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setResizable(false);
 
-        /* Add header */
-        mainPanel.add(getHeaderPanel());
-
-        /* 2 columns */
-        JPanel grid = new JPanel();
-        GridLayout gridLayout = new GridLayout(1, 2, 0, 0);
-        grid.setLayout(gridLayout);
-
-        /* add container selection at left column */
-        grid.add(getPackerPanel());
-
-        /* right column */
-        grid.add(getBoxesPanel());
-
-        mainPanel.add(grid);
-
-        frame.add(mainPanel);
+        return frame;
     }
 
     private JPanel getPackerPanel() {
-        JPanel packingOptionsPanel = new JPanel();
-        packingOptionsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        packingOptionsPanel.setPreferredSize(new Dimension(370, 500));
+        JPanel panel  = new JPanel();
+        panel.setLayout(new GridLayout(2, 1, 10, 10));
 
+        JPanel containerPanel = new JPanel(new BorderLayout());
         waveComboBox = new JComboBox();
         JLabel containerLabel = new JLabel("Kies container");
         containerLabel.setLabelFor(waveComboBox);
-        waveComboBox.setPreferredSize(new Dimension(370, 25));
+        containerPanel.add(containerLabel, BorderLayout.NORTH);
+        containerPanel.add(waveComboBox);
 
-        packingOptionsPanel.add(containerLabel);
-        packingOptionsPanel.add(waveComboBox);
-
+        JPanel timeoutPanel = new JPanel(new BorderLayout());
         timeOutComboBox = new JComboBox();
-        JLabel timeOutLabel = new JLabel("Kies maximale calculatie tijd in seconden");
-        timeOutLabel.setLabelFor(timeOutComboBox);
-        timeOutComboBox.setPreferredSize(new Dimension(370, 25));
+        JLabel timeoutLabel = new JLabel("Kies maximale calculatie tijd in seconden");
+        containerLabel.setLabelFor(timeOutComboBox);
+        timeoutPanel.add(timeoutLabel, BorderLayout.NORTH);
+        timeoutPanel.add(timeOutComboBox);
 
-        packingOptionsPanel.add(timeOutLabel);
-        packingOptionsPanel.add(timeOutComboBox);
+        panel.add(timeoutPanel);
+        panel.add(containerPanel);
 
-        packButton = new JButton("Calculeer ideale posities dozen in container");
-        packButton.setVerticalTextPosition(SwingConstants.CENTER);
-        packButton.setActionCommand("pack");
-        packButton.setToolTipText("Calculeer ideale posities dozen in container");
-        packButton.setPreferredSize(new Dimension(370, 25));
-
-        packingOptionsPanel.add(packButton);
-
-        return packingOptionsPanel;
+        return panel;
     }
 
     private JPanel getHeaderPanel() {
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-        panel.setSize(new Dimension(780, 9));
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0.5;
+        constraints.weighty = 1.5;
+        constraints.gridy = 0;
+        constraints.ipadx = 0;
+        constraints.gridwidth = 1;
+        constraints.anchor = GridBagConstraints.PAGE_START;
+        constraints.insets = new Insets(0,0,0,0);
 
         ImageIcon mainFreightLogo = new ImageIcon(getClass().getResource("/mainfreight-logo.png").getPath());
         JLabel label = new JLabel(mainFreightLogo);
-        label.setPreferredSize(new Dimension(780, 30));
-        panel.add(label);
+
+        panel.add(label, constraints);
 
         return panel;
     }
 
     private JPanel getBoxesPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         boxesTable = new JTable(null, null, null);
         boxesTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        boxesTable.setPreferredSize(new Dimension(800, 400));
+        boxesTable.setPreferredScrollableViewportSize(boxesTable.getPreferredSize());
+        boxesTable.setFillsViewportHeight(false);
+
+        JScrollPane listScroller = new JScrollPane(boxesTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        listScroller.setPreferredSize(new Dimension(800, 400));
+        panel.add(listScroller);
+
+        return panel;
+    }
+
+    public JPanel getOptionsPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0.5;
+        constraints.weighty = 1.5;
+        constraints.gridy = 0;
+        constraints.ipadx = 0;
+        constraints.gridwidth = 3;
+        constraints.anchor = GridBagConstraints.PAGE_END;
+        constraints.insets = new Insets(10,0,0,0);
+
+        packButton = new JButton("Calculeer ideale posities dozen in container");
+        packButton.setVerticalTextPosition(SwingConstants.CENTER);
+        packButton.setActionCommand("pack");
+        packButton.setToolTipText("Calculeer ideale posities dozen in container");
 
         addRowButton = new JButton("Voeg nieuwe doos toe");
         addRowButton.setVerticalTextPosition(SwingConstants.CENTER);
         addRowButton.setActionCommand("addBox");
         addRowButton.setToolTipText("Voeg nieuwe doos toe");
-        addRowButton.setSize(new Dimension(380, 20));
 
         removeRowButton = new JButton("Verwijder geselecteerde doos");
         removeRowButton.setVerticalTextPosition(SwingConstants.CENTER);
         removeRowButton.setActionCommand("removeBox");
         removeRowButton.setToolTipText("Verwijder geselecteerde doos");
-        removeRowButton.setSize(new Dimension(380, 20));
 
-        JLabel boxesLabel = new JLabel("Te berekenen pakketten");
-        boxesLabel.setLabelFor(boxesTable);
+        panel.add(removeRowButton, constraints);
+        panel.add(addRowButton, constraints);
+        panel.add(packButton, constraints);
 
-        JScrollPane listScroller = new JScrollPane(boxesTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        listScroller.setPreferredSize(new Dimension(380, 500));
-        boxesTable.setPreferredScrollableViewportSize(listScroller.getPreferredSize());
-        listScroller.setAlignmentX(LEFT_ALIGNMENT);
-
-        JPanel listPane = new JPanel();
-        listPane.setLayout(new BoxLayout(listPane, PAGE_AXIS));
-        listPane.setBorder(new LineBorder(new Color(0,0,0), 1));
-        listPane.add(boxesLabel);
-        listPane.add(Box.createRigidArea(new Dimension(0,5)));
-        listPane.add(listScroller);
-        listPane.add(Box.createRigidArea(new Dimension(0, 5)));
-
-        JPanel boxActionPanel = new JPanel();
-        boxActionPanel.setBorder(new LineBorder(new Color(0,0,0), 1));
-        boxActionPanel.setLayout(new BoxLayout(boxActionPanel, BoxLayout.PAGE_AXIS));
-        boxActionPanel.add(removeRowButton);
-        boxActionPanel.add(addRowButton);
-
-        listPane.add(boxActionPanel);
-        listPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-
-        return listPane;
+        return panel;
     }
 
     public JComboBox getWaveComboBox() {
