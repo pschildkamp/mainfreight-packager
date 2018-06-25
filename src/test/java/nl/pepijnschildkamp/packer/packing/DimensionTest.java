d5f740beee58db4148e0a435558cca57234f3378
Pepijn Schildkamp <pepijnschildkamp@pepijns-mbp.home>
2018-06-25 19:22:06 +0200

    Added dimension unit tests

--- src/test/java/nl/pepijnschildkamp/packer/packing/DimensionTest.java
+++ src/test/java/nl/pepijnschildkamp/packer/packing/DimensionTest.java
@@ -2,17 +2,43 @@ package nl.pepijnschildkamp.packer.packing;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DimensionTest {
-//    public boolean canHold2D(int w, int d, int h) {
     @Test
-    public void canHold2D() {
-
+    public void canHold2DHappyFlow() {
+        Dimension dimension = new Dimension("happyFlow", 5, 5, 5);
+        assertThat(dimension.canHold2D(5, 5, 3)).isEqualTo(true);
     }
-//    public boolean fitsInside3D(int w, int d, int h) {
-    @Test
-    public void fitsInside3D() {
 
+    @Test
+    public void canHold2DRotate() {
+        Dimension dimension = new Dimension("Rotate", 5, 10, 5);
+        assertThat(dimension.canHold2D(10, 5, 5)).isEqualTo(true);
+    }
+
+    @Test
+    public void canHold2DHeightTooLarge() {
+        Dimension dimension = new Dimension("tooLarge", 5, 5, 5);
+        assertThat(dimension.canHold2D(4, 3, 6)).isEqualTo(false);
+    }
+
+    @Test
+    public void fitsInside3DHappyFlow() {
+        Dimension dimension = new Dimension("happyFlow", 5, 5, 5);
+        assertThat(dimension.fitsInside3D(10, 10, 5)).isEqualTo(true);
+    }
+
+    @Test
+    public void fitsInside3DTooLarge() {
+        Dimension dimension = new Dimension("tooLarge", 5, 5, 5);
+        assertThat(dimension.fitsInside3D(5, 4, 3)).isEqualTo(false);
     }
 
     // isEmpty
+    @Test
+    public void isEmptyHappyFlow() {
+        Dimension dimension = new Dimension("happyFlow", 0,0,0);
+        assertThat(dimension.isEmpty()).isEqualTo(true);
+    }
 }
